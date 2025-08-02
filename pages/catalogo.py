e770c9e796ae429071f8846405b27d4492495fde
Bruxly <oskarmajo@hotmail.com>
2025-08-02 18:45:06 -0500

    Update catalogo.py

--- pages/catalogo.py
+++ pages/catalogo.py
@@ -1180,21 +1180,22 @@ if products:
             with col5:
                 st.markdown(
                     f"""
-                   <button style="
-                        background: linear-gradient(45deg, #667eea, #764ba2);
-                        color: white;
-                        border: none;
-                        border-radius: 25px;
-                        padding: 0.75rem 2rem;
-                        font-weight: bold;
-                        cursor: pointer;
-                    ">
-                        Pagar el producto
-                    </button>
-
-                        """,
+                    <a href="https://checkout.wompi.co/l/VPOS_s3EEBF" target="_blank">
+                       <button style="
+                            background: linear-gradient(45deg, #667eea, #764ba2);
+                            color: white;
+                            border: none;
+                            border-radius: 25px;
+                            padding: 0.75rem 2rem;
+                            font-weight: bold;
+                            cursor: pointer;
+                        ">
+                            Pagar el producto
+                        </button>
+                    </a>
+                    """,
                         unsafe_allow_html=True
-                    )
+                )
 
 
     
