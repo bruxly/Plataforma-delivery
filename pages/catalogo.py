be9f6add9341314d96bec036b6dc2929cc911093
Bruxly <oskarmajo@hotmail.com>
2025-08-02 21:04:59 -0500

    Update catalogo.py

--- pages/catalogo.py
+++ pages/catalogo.py
@@ -887,8 +887,7 @@ def get_products():
                 st.session_state.db.collection('products').add(product)
                 
             return sample_products
-            unique = {(p["name"], p["image"], p["category"]): p for p in productos}
-            productos = list(unique.values())
+           
         return products
 
     except Exception as e:
